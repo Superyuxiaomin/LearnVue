93121dff415160e850fe39bbb37cde87db62e40b
Yu <1358224343@qq.com>
2021-05-18 18:13:54 +0800

    LearnVue

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 
+
+const app = createApp(App)
+app.use(ElementPlus)
+app.mount('#app')
 createApp(App).use(router).mount('#app')
+
+
+
+
+
